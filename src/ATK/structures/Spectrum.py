b4eefab4c9198cf52058cc3701c178f0c7d29530
Ethan Moorfield <ethan.moorfield@hotmail.co.uk>
2026-01-29 18:59:46 +0000

    spectral fitting minimal working version

--- src/ATK/structures/Spectrum.py
+++ src/ATK/structures/Spectrum.py
@@ -18,9 +18,37 @@ class Spectrum(Container):
 
     # --- data ---
     wavelength: numpy.ndarray | QuantityArray | None = None
+    velocity: numpy.ndarray | QuantityArray | None = None
     flux: numpy.ndarray | QuantityArray | None = None
 
-    _required: tuple[str] = ("wavelength", "flux")
+    _required: tuple[str] = "flux"
+
+    def __post_init__(self):
+        # check for a valid input combination
+        if (self.wavelength is None) == (self.velocity is None):
+            raise ValueError("Spectrum container must hold one of 'wavelength' and 'velocity'.")
+
+        # ensure valid combination of wavelength/velocity
+        if self.velocity is None:
+            self.__dict__.pop("velocity", None)
+        if self.wavelength is None:
+            self.__dict__.pop("wavelength", None)
+
+    @property
+    def x_type(self):
+        if self.wavelength is not None:
+            return "wavelength"
+        elif self.velocity is not None:
+            return "velocity"
+        else:
+            raise ValueError("Spectrum container must hold one of 'wavelength' and 'velocity'.")
+
+    @property
+    def x_arr(self):
+        return getattr(self, self.x_type)
+
+    def set_x(self, val: numpy.ndarray):
+        setattr(self, self.x_type, val)
 
     def crop(self, min: float | None = None, max: float | None = None, inplace=True):
         from .methods.cropping import crop_nd
@@ -29,9 +57,9 @@ class Spectrum(Container):
 
         ys = [struct.flux]
 
-        x, ys = crop_nd(x=struct.wavelength, ys=ys, lower_lim=min, upper_lim=max)
+        x, ys = crop_nd(x=struct.x_arr, ys=ys, lower_lim=min, upper_lim=max)
 
-        struct.wavelength = x
+        struct.set_x(x)
         struct.flux = ys[0]
 
         return struct
@@ -43,14 +71,24 @@ class Spectrum(Container):
 
         ys = [struct.flux]
 
-        x, ys, _ = bin_nd(x=struct.wavelength, ys=ys, errs=[], bins=bins, size=size)
+        x, ys, _ = bin_nd(x=struct.x_arr, ys=ys, errs=[], bins=bins, size=size)
 
-        struct.wavelength = x
+        struct.set_x(x)
         struct.flux = ys[0]
 
         return struct
 
     def rv_fit(self, feature_wavelengths: list[float], feature_widths: list[float]):
-        from .methods.spectrum.fitting_2 import do_fitting
+        from .methods.spectrum.fitting import do_fitting
 
         do_fitting(self, feature_wavelengths, feature_widths)
+
+    def vspec(self, wav_ref: float | Quantity, inplace: bool = True):
+        from .methods.spectrum.fitting import get_velocities
+
+        struct = manage_inplace(self, inplace)
+
+        struct.velocity = get_velocities(struct.wavelength, wav_ref)
+        struct.wavelength = None
+
+        return struct
